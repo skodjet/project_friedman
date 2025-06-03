407f28ecdfc9e0beecd49711c758aca96f4c471c
Thomas Skodje <thomas.skodje@gmail.com>
2025-06-03 15:37:10 -0600

    roadmap entries class

--- app.py
+++ app.py
@@ -5,5 +5,24 @@ from datetime import datetime
 from datetime import timezone
 
 app = Flask(__name__)
+Scss(app)
 
 # Configure SQLAlchemy
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///roadmap.csv"
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+db = SQLAlchemy(app)
+
+# Data class for each roadmap entry
+class RoadmapEntry(db.Model):
+    external_title = db.Column(db.String(255))
+    internal_title = db.Column(db.String(255))
+    length = db.Column(db.Integer)
+    url = db.Column(db.String(255), primary_key=True)
+    # TODO: Figure out how JSON works in SQLAlchemy and Flask
+    category = db.Column(db.JSON)
+    ordering = db.Column(db.Integer)
+
+    def __repr__(self):
+        return self.internal_title
+
+
