25ea807dff727089acac810a88e074118b3d94ab
Thomas Skodje <thomas.skodje@gmail.com>
2025-06-15 17:47:45 -0600

    Table slide animation and close button. More formatting

--- static/roadmap_script.js
+++ static/roadmap_script.js
@@ -1,7 +1,18 @@
 const beginners_start_here_button = document.getElementById('beginners-start-here');
 const beginners_start_here_sidebar = document.getElementById('beginners-start-here-sidebar');
+const exit_buttons = document.querySelectorAll('.exit-button');
 
 beginners_start_here_button.addEventListener('click', () => {
-    beginners_start_here_sidebar.classList.remove('hidden');
-    beginners_start_here_sidebar.classList.add('shown');
+    beginners_start_here_sidebar.classList.toggle('shown');
 });
+
+// Setup for exit buttons
+exit_buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const shown_sidebar = document.querySelector('.shown');
+        shown_sidebar.classList.toggle('shown');
+    });
+});
+
+
+
