668fc567044d2ef82b9e022d3649350b567db546
skodjet <thomas.skodje@gmail.com>
2026-03-07 16:26:02 -0500

    new environment for desktop. signup.html init

--- static/roadmap_script.js
+++ static/roadmap_script.js
@@ -1,47 +1,3 @@
-// Sidebar exit button functionality
-const arrows = document.querySelectorAll('.arrow');
-const exit_buttons = document.querySelectorAll('.exit-button');
-exit_buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        const shown_sidebar = document.querySelector('.shown');
-        shown_sidebar.classList.remove('shown');
-        
-        arrows.forEach(arrow => {
-            arrow.classList.remove('hidden');
-        })
-    });
-});
-
-// Beginners start here
-// const beginners_start_here_button = document.getElementById('beginners-start-here');
-// const beginners_start_here_sidebar = document.getElementById('beginners-start-here-sidebar');
-// const bsh_progress_bar = document.getElementById('bsh-progress'); // Progress bar
-// const bsh_checkboxes = document.querySelectorAll('.bsh-checkbox'); // Checkboxes for each module
-// const num_bsh_modules = bsh_checkboxes.length;
-// let bsh_modules_completed = 0; // Number of checkboxes checked
-// let bsh_completed_count = document.getElementById('bsh-completed-count'); // Text
-
-// beginners_start_here_button.addEventListener('click', () => {
-//     beginners_start_here_sidebar.classList.add('shown');
-//     arrows.forEach(arrow => {
-//         arrow.classList.add('hidden');
-//     })
-// });
-
-// bsh_checkboxes.forEach(bsh_checkbox => {
-//     bsh_checkbox.addEventListener('click', () => {
-//         if (bsh_checkbox.checked) {
-//             bsh_modules_completed++;
-//             bsh_progress_bar.value++;
-//         } else {
-//             bsh_modules_completed--;
-//             bsh_progress_bar.value--;
-//         }
-//         bsh_completed_count.textContent = "(" + String(bsh_modules_completed) + "/" + String(num_bsh_modules) + ")";
-//     });
-// });
-
-
 // Functionality for showing sidebar
 // completed count = <p> element of how many modules are completed
 function setup_sidebar(button, sidebar, progress_bar, checkboxes, completed_count) {
@@ -71,6 +27,26 @@ function setup_sidebar(button, sidebar, progress_bar, checkboxes, completed_coun
     });
 }
 
+
+function navbar_setup(navbar_id) {
+    
+}
+
+
+// Sidebar exit button functionality
+const arrows = document.querySelectorAll('.arrow');
+const exit_buttons = document.querySelectorAll('.exit-button');
+exit_buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const shown_sidebar = document.querySelector('.shown');
+        shown_sidebar.classList.remove('shown');
+        
+        arrows.forEach(arrow => {
+            arrow.classList.remove('hidden');
+        })
+    });
+});
+
 // Beginners start here
 setup_sidebar(document.getElementById('beginners-start-here-button'), document.getElementById('beginners-start-here-sidebar'), 
               document.getElementById('bsh-progress'), document.querySelectorAll('.bsh-checkbox'), document.getElementById('bsh-completed-count'));
@@ -87,8 +63,12 @@ setup_sidebar(document.getElementById('picking-1-button'), document.getElementBy
 setup_sidebar(document.getElementById('artc-button'), document.getElementById('artc-sidebar'), 
               document.getElementById('artc-progress'), document.querySelectorAll('.artc-checkbox'), document.getElementById('artc-completed-count'));
 
-// TODO: Advanced picking
+// Advanced picking
 setup_sidebar(document.getElementById('picking-2-button'), document.getElementById('picking-2-sidebar'), 
               document.getElementById('pck2-progress'), document.querySelectorAll('.pck2-checkbox'), document.getElementById('pck2-completed-count'));
+
+
+// Navbar setup
+
 
 
