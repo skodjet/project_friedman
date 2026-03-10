74faacfcbd42887de58789b4fb6d0baf81ec7b0e
skodjet <thomas.skodje@gmail.com>
2026-03-10 19:51:11 -0400

    Fixing visual bugs. validation.js init

---
+++ static/validation.js
@@ -0,0 +1,8 @@
+const form = document.getElementById("login-form");
+const email_input = document.getElementById("email-input");
+const password_input = document.getElementById("password-input");
+const confirm_password_input = document.getElementById("confirm-password-input");
+
+form.addEventListener('submit', (e) => {
+    
+});
